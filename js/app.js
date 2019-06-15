369aef321bee09f927edbee084735be2b556c0fd
shaunhk <shaunhk@hotmail.com>
2019-06-15 22:29:27 +0100

    card updated/ game specific card backgrounds/ provisional background image

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const form = document.querySelector('#new-character');
   form.addEventListener('submit', handleFormSubmits);
+  form.addEventListener('click', switchPic);
 
   const deleteAll = document.querySelector('#delete');
   deleteAll.addEventListener('click', handleDelete);
@@ -24,11 +25,29 @@ const createGameCardItem = function(form){
   result.classList.add('game-item')
 
   const name = document.createElement('h3');
-  name.textContent = `Name: ${event.target.name.value}`;
+  name.textContent = `${event.target.name.value}`;
   result.appendChild(name);
 
   const game = document.createElement('h2');
   game.textContent = `${event.target.game.value}`;
+
+  if (game.textContent == "For Honor"){
+    result.classList.remove('game-item')
+    result.classList.add('for-honor')
+
+  }else if (game.textContent == "Killer Instinct"){
+    result.classList.remove('game-item')
+    result.classList.add('killer-instinct')
+
+  }else if (game.textContent == "Mortal Kombat"){
+    result.classList.remove('game-item')
+    result.classList.add('mortal-kombat')
+
+  }else if (game.textContent == "Tekken"){
+    result.classList.remove('game-item')
+    result.classList.add('tekken')
+  }
+
   result.appendChild(game);
 
   const image = document.createElement('img')
@@ -44,17 +63,19 @@ const createGameCardItem = function(form){
   losses.textContent = `Losses: ${event.target.losses.value}`;
   result.appendChild(losses);
 
-  const setupRatio = document.createElement('h4');
-  setupRatio.textContent = `Win / Loss Ratio:`;
-  result.appendChild(setupRatio)
-
-  const ratio = document.createElement('h3');
-  ratio.textContent = `${event.target.wins.value / event.target.losses.value}`;
+  const ratio = document.createElement('h4');
+  ratio.textContent = `W/L Ratio: ${event.target.wins.value / event.target.losses.value}`;
   result.appendChild(ratio);
 
   return result
 
 };
+
+const switchPic = function(event){
+  result.getElementbyclass('body')
+  switchPic.classList.add('ryu')
+  result.appendChild(switchPic)
+}
 
 const handleDelete = function(event){
   const characterList = document.querySelector('#character-list');
